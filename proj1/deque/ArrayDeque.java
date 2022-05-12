799d01a97be9775e15a2d48bbc0eef30ccd95f53
YangHancheng98 <275008026@qq.com>
2022-05-13 06:54:50 +0800

    fixed some bugs and style errors (lazy for fix all styles)

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -1,5 +1,7 @@
 package deque;
 
+import java.util.Iterator;
+
 public class ArrayDeque<T> implements Deque<T> {
     private T[] items;
     private int size;
@@ -16,115 +18,126 @@ public class ArrayDeque<T> implements Deque<T> {
         this.nextLast = 0; //第0个位置
     }
 
-    private void resize(int cap){
+    private void resize(int cap) {
         T[] temp = (T[]) new Object[cap];
 
-        boolean copy_flag = false;
-        int target_count = 0;
-        int copy_pointer;
-        if (nextFirst+1 >= this.capacity){
-            copy_pointer = 0;
+        boolean copyFlag = false;
+        int targetCount = 0;
+        int copyPointer;
+        if (nextFirst + 1 >= this.capacity) {
+            copyPointer = 0;
+        } else {
+            copyPointer = nextFirst + 1;
         }
-
-        else copy_pointer = nextFirst + 1;
 
         // 遍历打印数组直到尾巴，其实就是nextLast的前一位
-        int copy_end;
-        if(nextLast-1 < 0){
-            copy_end = this.capacity - 1;
+        int copyEnd;
+        if (nextLast - 1 < 0) {
+            copyEnd = this.capacity - 1;
+        } else {
+            copyEnd = nextLast - 1;
         }
-        else copy_end = nextLast - 1;
 
-        while (copy_flag == false){
-            temp[target_count] = items[copy_pointer];
-            if(copy_pointer == copy_end)
-                copy_flag = true;
-            if (copy_pointer + 1 == this.capacity)
-                copy_pointer = 0;
-            else copy_pointer++;
-            target_count++;
+        while (!copyFlag) {
+            temp[targetCount] = items[copyPointer];
+            if (copyPointer == copyEnd) {
+                copyFlag = true;
+            }
+            if (copyPointer + 1 == this.capacity) {
+                copyPointer = 0;
+            } else {
+                copyPointer++;
+            }
+            targetCount++;
         }
         items = temp;
         this.capacity = items.length;
         this.nextFirst = capacity - 1;
-        this.nextLast = target_count;
+        this.nextLast = targetCount;
     }
 
-    public void addFirst(T item){
-        if(size + 1 > capacity){
-            resize(2*capacity);
+    public void addFirst(T item) {
+        if (size + 1 > capacity) {
+            resize(2 * capacity);
         }
         items[nextFirst] = item;
-        if(nextFirst - 1 < 0){
+        if (nextFirst - 1 < 0) {
             nextFirst = capacity - 1;
+        } else {
+            nextFirst--;
         }
-        else nextFirst--;
         size++;
     }
 
-    public void addLast(T item){
-        if(size + 1 > capacity){
-            resize(2*capacity);
+    public void addLast (T item){
+        if (size + 1 > capacity) {
+            resize(2 * capacity);
         }
         items[nextLast] = item;
-        if(nextLast + 1 == capacity){
+        if (nextLast + 1 == capacity) {
             nextLast = 0;
+        } else {
+            nextLast++;
         }
-        else nextLast++;
         size++;
     }
 
 // 需要处理first在last后面的情况
     public void printDeque(){
-        if(size == 0){
+        if (size == 0) {
             return;
         }
-        int print_pointer;
-        if (nextFirst+1 >= capacity){
-            print_pointer = 0;
+        int printPointer;
+        if (nextFirst + 1 >= capacity) {
+            printPointer = 0;
+        } else {
+            printPointer = nextFirst + 1;
         }
-
-        else print_pointer = nextFirst + 1;
 
         // 遍历打印数组直到尾巴，其实就是nextLast的前一位
-        int print_end;
-        if(nextLast-1 < 0){
-            print_end = capacity - 1;
+        int printEnd;
+        if (nextLast - 1 < 0) {
+            printEnd = capacity - 1;
+        } else {
+            printEnd = nextLast - 1;
         }
-        else print_end = nextLast - 1;
 
-        boolean quit_flag = false;
+        boolean quitFlag = false;
         do {
-            System.out.print(items[print_pointer] + " ");
-            if(print_pointer == print_end)
-                quit_flag = true;
-            if (print_pointer + 1 == capacity)
-                print_pointer = 0;
-            else print_pointer++;
-        }while (quit_flag == false);
+            System.out.print(items[printPointer] + " ");
+            if (printPointer == printEnd) {
+                quitFlag = true;
+            }
+            if (printPointer + 1 == capacity) {
+                printPointer = 0;
+            } else {
+                printPointer++;
+            }
+        } while (!quitFlag);
         System.out.print("\n");
     }
 
     public T removeFirst(){
-        if(size == 0){
+        if (size == 0) {
             return null;
         }
 
-        int remove_pointer;
-        if (nextFirst+1 == capacity){
-            remove_pointer = 0;
+        int removePointer;
+        if (nextFirst + 1 == capacity) {
+            removePointer = 0;
+        } else {
+            removePointer = nextFirst + 1;
         }
-        else remove_pointer = nextFirst + 1;
 
-        T return_value = items[remove_pointer];
-        items[remove_pointer] = null;
+        T returnValue = items[removePointer];
+        items[removePointer] = null;
         size--;
-        nextFirst = remove_pointer;
+        nextFirst = removePointer;
         if(size >= 8 && size <= 0.5 * capacity)
             resize((int) (0.5*capacity));
 
 
-        return return_value;
+        return returnValue;
     }
 
     public T removeLast(){
@@ -132,18 +145,19 @@ public class ArrayDeque<T> implements Deque<T> {
             return null;
         }
 
-        int remove_pointer;
+        int removePointer;
         if(nextLast == 0){
-            remove_pointer = capacity - 1;
+            removePointer = capacity - 1;
         }
-        else remove_pointer = nextLast - 1;
+        else removePointer = nextLast - 1;
 
-        T return_value = items[remove_pointer];
-        items[remove_pointer] = null;
+        T return_value = items[removePointer];
+        items[removePointer] = null;
         size--;
-        nextLast = remove_pointer;
-        if(size >= 8 && size <= 0.5 * capacity)
-            resize((int) (0.5*capacity));
+        nextLast = removePointer;
+        if (size >= 8 && size <= 0.5 * capacity) {
+            resize((int) (0.5 * capacity));
+        }
 
         return return_value;
     }
@@ -162,6 +176,48 @@ public class ArrayDeque<T> implements Deque<T> {
         get_pointer %= capacity;
 
         return items[get_pointer];
+    }
+    protected int addOne(int index) {
+        return (index + 1) % items.length;
+    }
+
+    public Iterator<T> iterator(){
+        return new ArrayDeque.ArrayDequeIterator();
+    }
+
+    private class ArrayDequeIterator implements Iterator<T>{
+        private int ptr;
+        ArrayDequeIterator(){
+            ptr = addOne(nextFirst);
+        }
+        public boolean hasNext() {
+            return (ptr != nextLast);
+        }
+        public T next() {
+            T value = items[ptr];
+            ptr = addOne(ptr);
+            return value;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Deque)) {
+            return false;
+        }
+        Deque other = (Deque) o;
+        if (size != other.size()) {
+            return false;
+        }
+
+        int index = addOne(nextFirst);
+        for (int i = 0; i < size; i++) {
+            if (!(items[index].equals(other.get(i)))) {
+                return false;
+            }
+            index = addOne(index);
+        }
+        return true;
     }
 
     public static void main(String[] args) {
