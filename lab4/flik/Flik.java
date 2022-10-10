be0c5c967ffc65da89e77ed0148434f4841205c0
YangHancheng98 <275008026@qq.com>
2022-10-10 01:31:46 -0400

    Done with lab4

--- lab4/flik/Flik.java
+++ lab4/flik/Flik.java
@@ -8,6 +8,9 @@ public class Flik {
      *  @param b Value 2
      *  @return Whether a and b are the same */
     public static boolean isSameNumber(Integer a, Integer b) {
-        return a == b;
+        return a.equals(b);
     }
+
+    //(a=127,位置@696,b=127,位置@696,a=128,位置@697,b=128,位置@698)
+    //==比较两个对象的内存地址是否相同，.equals比较两个对象的内容是否相同
 }
